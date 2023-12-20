444e55f72118d033a5492045b488d95152725a49
Lucca Wang <luccawang@gmail.com>
2023-12-20 14:10:15 -0700

    Part 1 complete, fortune is in the div

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -4,8 +4,14 @@
 
 function showFortune(evt) {
   // TODO: get the fortune and show it in the #fortune-text div
+  fetch('/fortune')
+    .then((response) => response.text())
+    .then((serverData) => {
+      console.log('')
+      document.querySelector('#fortune-text').innerHTML = serverData
+    })
 }
-
+  
 document.querySelector('#get-fortune-button').addEventListener('click', showFortune);
 
 // PART 2: SHOW WEATHER
