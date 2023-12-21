2b4919f138421bdb910d36fe656c6bd10631390b
Lucca Wang <luccawang@gmail.com>
2023-12-20 19:53:27 -0700

    Saving work, part 2 complete, on the cusp of part 3

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -14,25 +14,66 @@ function showFortune(evt) {
   
 document.querySelector('#get-fortune-button').addEventListener('click', showFortune);
 
+
 // PART 2: SHOW WEATHER
 
 function showWeather(evt) {
   evt.preventDefault();
 
-  const url = '/weather.json';
   const zipcode = document.querySelector('#zipcode-field').value;
-
+  const url = `/weather.json?zipcode=${zipcode}`;
   // TODO: request weather with that URL and show the forecast in #weather-info
-}
+  // we're returning a json object (like a dictionary in Python)
+  // based on the zipcode number entered by the user, we're taking that entry and sending it back to the server 
+  // then based on what we get back, we want to display the DEFAULT_WEATHER key ('forecast')  and also the value of that key 
+    
+  fetch(url)
+      .then((response) => response.json())
+      .then((serverData) => {
+        //we only want the key/property at 'forecast' and we do not want temp
+        console.log(serverData['forecast'])
+        document.querySelector('#weather-info').innerHTML = serverData['forecast']
+      })
+  }
 
 document.querySelector('#weather-form').addEventListener('submit', showWeather);
 
+
 // PART 3: ORDER MELONS
+  // TODO: show the result message after your form
+  // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+
+  // <div id="order-status"></div>
+  // we get back the JSON object (like a python dictionary)
+  // we have the div that we need to put our data into 
+
+  function updateMelons(results) {
+    if (results.code === 'OK') {
+      document.querySelector('#order-status').classList.remove('order-error');
+      document.querySelector('#order-status').innerHTML = `<p>${results.msg}</p>`;
+    } else {
+      document.querySelector('#order-status').classList.add('order-error');
+      document.querySelector('#order-status').innerHTML = `<p><b>${results.msg}</b></p>`;
+    }
+  }
 
 function orderMelons(evt) {
   evt.preventDefault();
-
-  // TODO: show the result message after your form
-  // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
-}
+  
+  const formInputs = {
+    melon_type: document.querySelector('#melon-type-field').value,
+    qty: document.querySelector('#qty-field').value,
+  };
+  
+  fetch('/order-melons.json', {
+    method: 'POST',
+    body: JSON.stringify(formInputs),
+    header: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) = response.json())
+    .then(updateMelons);
+  }
+    
 document.querySelector('#order-form').addEventListener('submit', orderMelons);
